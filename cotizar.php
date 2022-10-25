dd6662a18a1d221001272af86fa870e19d499f84
eivxr <110433232+eivxr@users.noreply.github.com>
2022-10-24 23:28:29 -0500

    Add files via upload

---
+++ cotizar.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ISSSSS CLUB | Servicios</title>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+    rel="stylesheet" 
+    integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" 
+    crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="css/normalize.css" />
+    <link rel="stylesheet" href="css/styles.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <!--Fuentes-->
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter+Tight&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Oswald:wght@600&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter+Tight&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap"
+      rel="stylesheet"
+    />
+    <!--icono-->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+      integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    /> 
+</head>
+<link rel="icon" href="img/logo1.png" />
+<body>
+<header class="header">
+      <div class="logo">
+        <a href="index.html"><img src="img/logo1.png" alt="Logo marca" /></a>
+      </div>
+      <nav>
+        <ul class="nav-links">
+          <li><a href="index.html">Inicio</a></li>
+          <li><a href="galeria.html">Galería</a></li>
+          <li><a href="about.html">Acerca de</a></li>
+          <li><a href="contacto.html">Contact</a></li>
+        </ul>
+      </nav>
+      <form action="index.php" class="btn" method="POST">
+        <button>Cotizar</button></a>
+      </form>
+
+      <a onclick="openNav()" class="menu" href="#"><button>Menu</button></a>
+
+      <div id="mob-men" class="overlay">
+        <a onclick="closeNav()" href="#" class="close">&times;</a>
+        <div class="overlayContent">
+          <li><a href="index.html">Inicio</a></li>
+          <li><a href="galeria.html">Galería</a></li>
+          <li><a href="about.html">Acerca de</a></li>
+          <li><a href="contSacto.html">Contact</a></li>
+          <li><a href="cotizar.html" class="btn">Cotizar</a></li>
+        </div>
+      </div>
+    </header>
+    <footer>
+      <img src="img/logo3.jpg" alt="logo" class="logo" />
+      <div class="iconos-social-cont">
+        <a href="https://www.facebook.com/IsssssClub" class="icono-social"></a>
+        <a
+          href="https://www.instagram.com/isssssclub/"
+          class="icono-social"
+        ></a>
+      </div>
+
+      <ul class="menu-footer">
+        <li class="menu-item"><a href="">Ayuda</a></li>
+        <li class="menu-item"><a href="">Privacidad</a></li>
+        <li class="menu-item"><a href="contacto.html">Reportar</a></li>
+      </ul>
+      <span class="copyright"
+        >&copy2022, Rubix.Inc ,Todos los derechos reservados</span
+      >
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" 
+    crossorigin="anonymous"></script>
+</body>
+</html>
